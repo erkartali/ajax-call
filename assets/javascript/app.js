4840c092f1081dfacf2aef9df5aa3349048f4c84
Eric Kartali <erkartali@gmail.com>
2020-04-08 18:31:03 -0500

    fixed the buttons not producing gifs on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,118 +1,100 @@
-
 var gifArray = [];
 
-$('.search-button').click(function (event) {
-    var userInput = $("#image-input").val();
-    event.preventDefault();
-    
-    // Constructing a queryURL using the user input
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + userInput + '&api_key=4xhpDEomHg5DzxEn208KYSwlZIcF6lzd&limit=10&rating=g';
-    
-    // Performing an AJAX request with the queryURL
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    .then(function (response) {
-    console.log(queryURL);
+$(".search-button").click(function(event) {
+  var userInput = $("#image-input").val();
+  event.preventDefault();
 
-        console.log(response.data);
-        var results = response.data;
+  // Constructing a queryURL using the user input
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    userInput +
+    "&api_key=4xhpDEomHg5DzxEn208KYSwlZIcF6lzd&limit=5&rating=g";
 
-        for (var i = 0; i < results.length; i++) {
-            var imageDiv = $("<div>");
-            var p = $("<p>").text("Rating: " + results[i].rating);
-            var imageThing = $("<img>").addClass('gif');
-            imageThing.attr("src", results[i].images.fixed_height_still.url);
-            imageThing.attr("data-still", results[i].images.fixed_height_still.url);
-            imageThing.attr("data-animate", results[i].images.fixed_height.url);
-            imageThing.attr("data-state", "still");
+  // Performing an AJAX request with the queryURL
+  generateGifs(userInput);
 
-            imageDiv.append(imageThing);
-            imageDiv.append(p);
+  $("#image-input").val("");
 
-            $(".third").prepend(imageDiv);
-            whatever();
-        }
-
-    });
-    $('#image-input').val('');
-    
-    if (userInput.val != '' && userInput != '') {
-        gifArray.push(userInput);
-        renderButtons()
-    } else {
-        alert('You gotta type something bro.')
-    }
+  if (userInput.val != "" && userInput != "") {
+    gifArray.push(userInput);
+    renderButtons();
+  } else {
+    alert("You gotta type something bro.");
+  }
 });
 
 function renderButtons() {
-    console.log(gifArray);
-    $(".button-div").empty();
+  console.log(gifArray);
+  $(".button-div").empty();
 
-    for (var i = 0; i < gifArray.length; i++) {
+  for (var i = 0; i < gifArray.length; i++) {
+    var a = $("<button>");
+    a.addClass("image-button button");
+    a.attr("data-name", gifArray[i]);
+    a.text(gifArray[i]);
 
-      var a = $("<button>");
-      a.addClass("image-button button");
-      a.attr("data-name", gifArray[i]);
-      a.text(gifArray[i]);
-      
-      $(".button-div").append(a); 
-    }
+    $(".button-div").append(a);
+  }
+
+  $(".image-button").on('click', function() {
+    var buttonName = $(this).attr("data-name");
+    generateGifs(buttonName);
+  });
 }
 
+function generateGifs(gifName) {
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    gifName +
+    "&api_key=4xhpDEomHg5DzxEn208KYSwlZIcF6lzd&limit=5&rating=g";
 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+      var imageDiv = $("<div>");
+      var p = $("<p>").text("Rating: " + results[i].rating);
+      var imageThing = $("<img>").addClass("gif");
+      imageThing.attr("src", results[i].images.fixed_height.url);
+      imageThing.attr("data-still", results[i].images.fixed_height_still.url);
+      imageThing.attr("data-animate", results[i].images.fixed_height.url);
+      imageThing.attr("data-state", "animate");
+
+      imageDiv.append(imageThing);
+      imageDiv.append(p);
+
+      $(".third").prepend(imageDiv);
+    }
+    whatever();
+  });
+}
 
 function alertGifName() {
-    var gifName = $(this).attr("data-name");
-    renderButtons();
-    // alert(gifName);
+  var gifName = $(this).attr("data-name");
+  renderButtons();
 
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + gifName + '&api_key=4xhpDEomHg5DzxEn208KYSwlZIcF6lzd&limit=5&rating=g';
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    .then(function (response) {
-        var results = response.data;
-
-        for (var i = 0; i < results.length; i++) {
-            var imageDiv = $("<div>");
-            var p = $("<p>").text("Rating: " + results[i].rating);
-            var imageThing = $("<img>").addClass('gif');
-            imageThing.attr("src", results[i].images.fixed_height.url);
-            imageThing.attr("data-still", results[i].images.fixed_height_still.url);
-            imageThing.attr("data-animate", results[i].images.fixed_height.url);
-            imageThing.attr("data-state", 'animate');
-
-            imageDiv.append(imageThing);
-            imageDiv.append(p);
-
-            $(".third").prepend(imageDiv);
-        }
-        whatever();
-    });
+  generateGifs(gifName);
 }
 
 $("#search-button").on("click", function(event) {
-    event.preventDefault();
-    renderButtons();
+  event.preventDefault();
+  renderButtons();
 });
 
 function whatever() {
-    $(".gif").on("click", function() {
-        console.log('this');
-        var state = $(this).attr("data-state");
-    
-        if ($(this).attr("data-state") === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-        } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-        }
-    });
-    
-}
+  $(".gif").on("click", function() {
+    // console.log('this');
+    var state = $(this).attr("data-state");
 
+    if ($(this).attr("data-state") === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+}
